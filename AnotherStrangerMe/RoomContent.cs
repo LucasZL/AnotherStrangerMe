e3b9b57253dcb2afb5c900d0b5f6ed00d54b7071
LucasZL <lucas_1996@live.de>
2015-01-05 06:12:09 -0800

    First Upload
    First Upload

---
+++ AnotherStrangerMe/RoomContent.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AnotherStrangerMe
+{
+    class RoomContent
+    {
+        public static string[] roomArrayCommands;
+        public static string[] roomDescription;
+        public static string[] shortDescriptionArray;
+        static StreamReader arrayReader;
+        static StreamReader description;
+        static StreamReader shortdescription;
+        private static List<Room> roomList;
+
+        public RoomContent()
+        {
+            roomList = new List<Room>();
+            arrayReader = new StreamReader("objectList.txt");
+            description = new StreamReader("roomDescription.txt");
+            shortdescription = new StreamReader("shortRoomDescription.txt");
+            roomArrayCommands = arrayReader.ReadToEnd().Split(new Char[] { '|' });
+            roomDescription = description.ReadToEnd().Split(new Char[] { '|' });
+            shortDescriptionArray = shortdescription.ReadToEnd().Split(new Char[] { '|' });
+        }
+
+        public Room Room1()
+        {
+            addRoomsToList();
+            return roomList[0];
+        }
+
+        public void addRoomsToList()
+        {
+            for (int i = 0; i < roomArrayCommands.Length; i++)
+            {
+                roomList.Add(new Room(roomArrayCommands[i].Split(new Char[] { ',' }), roomDescription[i],shortDescriptionArray[i]));
+            }
+        }
+        
+
+        public static void NextRoom(int roomNumber, Room currentRoom, Player player)
+        {
+            player.room = null;
+            player.room = roomList[roomNumber];
+        }
+
+        
+
+    }
+}
---
+++ ReadMe!!!!!!!.txt
@@ -0,0 +1,4 @@
+1. Es läuft alles über das Intro
+2. Die ClearTopLane methode funktioniert nicht richtig (eventuell findet ihr eine bessere Lösung)
+3. Die textdatein müssen noch gemacht werden
+4. Befehle wie untersuchen müssen noch implementiert werden
